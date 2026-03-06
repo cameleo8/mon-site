ba4f7d1e8780332355ca96a88c776cb4bdbc7539
kaeloo <147034168+kaeloo8@users.noreply.github.com>
2026-03-06 16:08:49 +0100

    update

--- page/script_parcour.js
+++ page/script_parcour.js
@@ -1,43 +1,3 @@
-// ==============================
-// ====== CAROUSEL =========
-// ==============================
-
-
-
-const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dot");
-const nextBtn = document.querySelector(".next");
-const prevBtn = document.querySelector(".prev");
-
-let current = 0;
-
-function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active"));
-    dots.forEach(dot => dot.classList.remove("active"));
-
-    slides[index].classList.add("active");
-    dots[index].classList.add("active");
-}
-
-nextBtn.addEventListener("click", () => {
-    current++;
-    if (current >= slides.length) current = 0;
-    showSlide(current);
-});
-
-prevBtn.addEventListener("click", () => {
-    current--;
-    if (current < 0) current = slides.length - 1;
-    showSlide(current);
-});
-
-dots.forEach((dot, i) => {
-    dot.addEventListener("click", () => {
-        current = i;
-        showSlide(current);
-    });
-});
-
 
 // ==============================
 // ====== HEX BACKGROUND ========
@@ -115,7 +75,7 @@ function drawBackground() {
 
 
 // ===== GRILLE HEXAGONALE =====
-const baseColor = { r: 0, g: 200, b: 120 }; // change la couleur ici
+const baseColor = { r: 180, g: 0, b: 255 }; // change la couleur ici
 
 function drawHexGrid() {
     for (let row = 0; row < Math.ceil(height / hexHeight) + 2; row++) {
